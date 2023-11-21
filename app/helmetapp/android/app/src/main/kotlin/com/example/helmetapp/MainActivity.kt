2d7305a18033c13104104488de0498aceb7e2308
Victor Wilberforce <victorwilberforce2310@gmail.com>
2023-11-21 14:26:49 -0500

    Starting over

--- app/helmetapp/android/app/src/main/kotlin/com/example/helmetapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.helmetapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- app/helmetapp/lib/mapview.dart
+++
@@ -1,1 +0,0 @@
-import 'package:google_maps_flutter/google_maps_flutter.dart';
